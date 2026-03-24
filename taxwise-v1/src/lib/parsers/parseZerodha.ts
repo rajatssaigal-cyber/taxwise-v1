a9ec755bc0d43b748a77838801e2ed00f0d7d0e0
rajatssaigal-cyber <rajatssaigal@gmail.com>
2026-03-24 16:30:16 +0530

    Create parseZerodha.ts

---
+++ taxwise-v1/src/lib/parsers/parseZerodha.ts
@@ -0,0 +1,6 @@
+import { InvestmentTransaction, AssetType } from '../../types/tax';
+
+export const parseZerodha = async (fileData: string): Promise<InvestmentTransaction[]> => {
+  // Logic to parse Zerodha P&L statements
+  return [];
+};
