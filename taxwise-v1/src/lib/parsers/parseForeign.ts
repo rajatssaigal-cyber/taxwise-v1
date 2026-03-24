c8d5e8063c7f7d75617cf8dd10d327490a093417
rajatssaigal-cyber <rajatssaigal@gmail.com>
2026-03-24 16:29:37 +0530

    Create parseForeign.ts

---
+++ taxwise-v1/src/lib/parsers/parseForeign.ts
@@ -0,0 +1,6 @@
+import { InvestmentTransaction, AssetType } from '../../types/tax';
+
+export const parseForeign = async (fileData: string): Promise<InvestmentTransaction[]> => {
+  // Logic to parse Foreign Asset statements
+  return [];
+};
